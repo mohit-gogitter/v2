89194645e9bff64cf40c644fc383cdc770cca186
Mohit Singh <mohitsingh20.dev@gmail.com>
2024-09-30 17:24:26 +0530

    updated css

--- app/globalcomponents/image_square.tsx
+++ app/globalcomponents/image_square.tsx
@@ -4,8 +4,8 @@ import MyPic from "../../public/images/my_pic_4.jpg";
 
 export default function MyImage() {
   return (
-    <div className="justify-center items-center mt-10 md:mt-0">
-      <div className="relative w-60 h-60 md:w-80 md:h-80 group">
+    <div className="justify-center items-center mt-9 md:mt-0">
+      <div className="relative w-64 h-64 md:w-80 md:h-80 group">
         {/* Stylish border */}
         <div className="absolute inset-0 bg-gradient-to-br from-cyan-900 to-gray-900 rounded-sm transform rotate-6 scale-105 group-hover:scale-110 transition duration-300"></div>
         
